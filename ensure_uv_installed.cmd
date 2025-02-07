9417a0ab7dfcf1ae3f59d2c921a049e95402696f
Jay Bazuzi <jay@bazuzi.com>
2025-02-06 23:05:59 -0800

    ^ F only install uv if not already installed

--- ensure_uv_installed.cmd
+++ ensure_uv_installed.cmd
@@ -1,6 +1,9 @@
 @echo off
 setlocal
 
-@REM Clear PsModulePath to avoid conflicts with PWSH, which happens in GitHub actions
-set "PsModulePath="
-powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex" || goto :ERROR
+where /q uv
+if ERRORLEVEL 1 (
+    @REM Clear PsModulePath to avoid conflicts with PWSH, which happens in GitHub actions
+    set "PsModulePath="
+    powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex" || goto :ERROR
+)
