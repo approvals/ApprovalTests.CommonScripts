f59dd35d737f71de3a92c29f355413f5047f864b
Llewellyn Falco <llewellyn.falco@gmail.com>
2025-02-09 19:13:53 +0000

    @ t WIP: approve_all script
    Co-Authored-By: Jay Bazuzi <jay@bazuzi.com>
    Co-Authored-By: Michael R Wolf <MichaelRWolf@att.net>
    Co-Authored-By: 4dsherwood <10386643+4dsherwood@users.noreply.github.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: Joss Hufnagel <43077216+blade290@users.noreply.github.com>
    Co-Authored-By: Tomer Negad <107677845+tomernegad@users.noreply.github.com>

--- test_.py
+++ test_.py
@@ -4,3 +4,23 @@ import pathlib
 # Not really a test, but a way to ensure that the tests are actually running in CI and didn't get skipped silently
 def test__ensure_tests_are_actually_running():
     pathlib.Path(__file__).parent.joinpath("semaphore").touch()
+
+
+def execute_the_script(param):
+    pass
+
+
+
+def test__canary():
+    # todo: add a sandbox
+    pathlib.Path("a").write_text("a contents!")
+    pathlib.Path("b").write_text("b contents!")
+    pathlib.Path("example_failed_comparison.log").write_text("a->b")
+
+    execute_the_script("example_failed_comparison.log")
+
+    # compare the first file to the second file
+    a_contents = pathlib.Path("a").read_text()
+    b_contents = pathlib.Path("b").read_text()
+    assert b_contents == a_contents
+
