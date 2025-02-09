51eafe6e4a67ec3c42481981b4b7c0d92d3988ca
Llewellyn Falco <llewellyn.falco@gmail.com>
2025-02-09 19:26:40 +0000

    . t r rename
    Co-Authored-By: Jay Bazuzi <jay@bazuzi.com>
    Co-Authored-By: Michael R Wolf <MichaelRWolf@att.net>
    Co-Authored-By: 4dsherwood <10386643+4dsherwood@users.noreply.github.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: Joss Hufnagel <43077216+blade290@users.noreply.github.com>
    Co-Authored-By: Tomer Negad <107677845+tomernegad@users.noreply.github.com>

--- test_.py
+++ test_.py
@@ -13,15 +13,15 @@ def execute_the_script(param):
 
 
 def test__canary():
-    with tempfile.TemporaryDirectory(prefix="ApprovalTests.CommonScripts-") as temp_dir:
-        pathlib.Path(temp_dir, "a").write_text("a contents!")
-        pathlib.Path(temp_dir, "b").write_text("b contents!")
-        pathlib.Path(temp_dir, "example_failed_comparison.log").write_text("a->b")
+    with tempfile.TemporaryDirectory(prefix="ApprovalTests.CommonScripts-") as sandbox:
+        pathlib.Path(sandbox, "a").write_text("a contents!")
+        pathlib.Path(sandbox, "b").write_text("b contents!")
+        pathlib.Path(sandbox, "example_failed_comparison.log").write_text("a->b")
 
         execute_the_script("example_failed_comparison.log")
 
         # compare the first file to the second file
-        a_contents = pathlib.Path(temp_dir, "a").read_text()
-        b_contents = pathlib.Path(temp_dir, "b").read_text()
+        a_contents = pathlib.Path(sandbox, "a").read_text()
+        b_contents = pathlib.Path(sandbox, "b").read_text()
     assert b_contents == a_contents
 
