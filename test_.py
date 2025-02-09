c1c1f10ba7aa440e78ab7231f7945aaecfe848e8
Llewellyn Falco <llewellyn.falco@gmail.com>
2025-02-09 19:21:23 +0000

    @ t RED: approve_all script
    Co-Authored-By: Jay Bazuzi <jay@bazuzi.com>
    Co-Authored-By: Michael R Wolf <MichaelRWolf@att.net>
    Co-Authored-By: 4dsherwood <10386643+4dsherwood@users.noreply.github.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>
    Co-Authored-By: Joss Hufnagel <43077216+blade290@users.noreply.github.com>
    Co-Authored-By: Tomer Negad <107677845+tomernegad@users.noreply.github.com>

--- test_.py
+++ test_.py
@@ -1,4 +1,5 @@
 import pathlib
+import tempfile
 
 
 # Not really a test, but a way to ensure that the tests are actually running in CI and didn't get skipped silently
@@ -12,15 +13,15 @@ def execute_the_script(param):
 
 
 def test__canary():
-    # todo: add a sandbox
-    pathlib.Path("a").write_text("a contents!")
-    pathlib.Path("b").write_text("b contents!")
-    pathlib.Path("example_failed_comparison.log").write_text("a->b")
+    with tempfile.TemporaryDirectory(prefix="ApprovalTests.CommonScripts-") as temp_dir:
+        pathlib.Path(temp_dir, "a").write_text("a contents!")
+        pathlib.Path(temp_dir, "b").write_text("b contents!")
+        pathlib.Path(temp_dir, "example_failed_comparison.log").write_text("a->b")
 
-    execute_the_script("example_failed_comparison.log")
+        execute_the_script("example_failed_comparison.log")
 
-    # compare the first file to the second file
-    a_contents = pathlib.Path("a").read_text()
-    b_contents = pathlib.Path("b").read_text()
+        # compare the first file to the second file
+        a_contents = pathlib.Path(temp_dir, "a").read_text()
+        b_contents = pathlib.Path(temp_dir, "b").read_text()
     assert b_contents == a_contents
 
