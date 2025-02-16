93e4aeff8c94e078488123a919a889eed4bd0d66
Llewellyn Falco <llewellyn.falco@gmail.com>
2025-02-16 18:46:52 +0000

    . t refactoring
    Co-Authored-By: Jay Bazuzi <jay@bazuzi.com>
    Co-Authored-By: Nitsan Avni <nitsanav@gmail.com>

--- test_approve_all.py
+++ test_approve_all.py
@@ -3,17 +3,17 @@ import pathlib
 import subprocess
 import sys
 import tempfile
+import shutil
 
 from approve_all import approve_all
 
 
-def execute_the_script(cwd: pathlib.Path):
+def execute_the_script(script: pathlib.Path):
     subprocess.run(
         [
             sys.executable,
-            ".approval_tests_temp/approve_all.py",
+            script
         ],
-        cwd=cwd,
         text=True,
         check=True,
     )
@@ -21,37 +21,25 @@ def execute_the_script(cwd: pathlib.Path):
 
 def test__end_to_end_test():
     with tempfile.TemporaryDirectory(prefix="ApprovalTests.CommonScripts-") as _sandbox:
-        template_folder = pathlib.Path("template_folder")
-        import shutil
-        shutil.copytree(template_folder, _sandbox, dirs_exist_ok=True)
-
+        shutil.copytree(pathlib.Path("template_folder"), _sandbox, dirs_exist_ok=True)
         sandbox = pathlib.Path(_sandbox)
-        a = sandbox / "a.received.txt"
-        received_text = a.read_text()
-        # copy sandef copy_and_replace_template(sandbox: pathlib.Path):
-        template_path = sandbox / ".approval_tests_temp/.failed_comparison.log.template"
-        log_path = sandbox / ".approval_tests_temp/.failed_comparison.log"
+        render_template(sandbox, ".approval_tests_temp/.failed_comparison.log.template")
+        script = sandbox / ".approval_tests_temp/approve_all.py"
+        shutil.copy(script.name, script)
 
-        # Read the template content
-        content = template_path.read_text()
-
-        # Replace {root} with the full path of the sandbox
-        print(f"{content=}")
-        content = content.replace("{root}", str(sandbox) + os.sep)
-        print(f"{content=}")
-
-        # Write the modified content to the log file
-        log_path.write_text(content)
-
-        (sandbox / ".approval_tests_temp/approve_all.py").write_text(
-            pathlib.Path("approve_all.py").read_text()
-        )
-
-        execute_the_script(cwd=sandbox)
-
+        received_text = (sandbox / "a.received.txt").read_text()
+        execute_the_script(script)
         approved_text = (sandbox / "a.approved.txt").read_text()
 
     assert received_text == approved_text
+
+
+def render_template(root_dir, template_path):
+    template_path = root_dir / template_path
+    log_path = template_path.with_name(template_path.name.replace(".template", ""))
+    content = template_path.read_text()
+    content = content.replace("{root}", str(root_dir) + os.sep)
+    log_path.write_text(content)
 
 
 def test__approve_all__with_loader_and_saver():
