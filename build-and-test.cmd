41decbf28eca51d802241652d1e40e587b9373c5
Jay Bazuzi <jay@bazuzi.com>
2025-03-31 10:32:04 -0700

    . e simplify batch file

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,4 +1,1 @@
-@echo off
-setlocal
-
-call ensure_uv_installed.cmd && call "%USERPROFILE%\.local\bin\uv" run -- python -m pytest . -v
+@call ensure_uv_installed.cmd && call "%USERPROFILE%\.local\bin\uv" run -- python -m pytest . -v
