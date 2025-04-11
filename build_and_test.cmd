91584a579bbeaf0cfe5c1156174e394044ee0a69
Jay Bazuzi <jay@bazuzi.com>
2025-04-11 14:08:07 -0700

    . e use a venv

--- build_and_test.cmd
+++ build_and_test.cmd
@@ -1,5 +1,8 @@
 @echo off
 
 call python --version
+python3 -m venv .venv
+call .venv\Scripts\activate.bat
+
 call python -m pip install --upgrade pip --requirement requirements.dev.txt
 call python -m pytest . -v
