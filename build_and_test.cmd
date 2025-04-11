a2e38ef6d703b03f5ab2e89cdd5176c6d9029ce3
Jay Bazuzi <jay@bazuzi.com>
2025-04-11 14:12:56 -0700

    . e run mypy in the build

--- build_and_test.cmd
+++ build_and_test.cmd
@@ -5,4 +5,4 @@ python3 -m venv .venv
 call .venv\Scripts\activate.bat
 
 call python -m pip install --upgrade pip --requirement requirements.dev.txt
-call python -m pytest . -v
+call python -m pytest . -v && python -m mypy
