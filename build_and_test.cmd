1fe64e450a827d32f1d18f202128a66e066a37bf
Jay Bazuzi <jay@bazuzi.com>
2025-04-13 09:20:32 -0700

    . e fix use of python3 on windows

--- build_and_test.cmd
+++ build_and_test.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 call python --version
-python3 -m venv .venv
+python -m venv .venv
 call .venv\Scripts\activate.bat
 
 call python -m pip install --upgrade pip --requirement requirements.dev.txt
